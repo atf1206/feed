59cea44152959126a3df5153a1761aa22af8221d
Andrew <atf245@nyu.edu>
2018-02-04 15:45:52 -0500

    Cleanup
    Fix indeed.png
    
    jsonp nyt

--- js/feed.js
+++ js/feed.js
@@ -58,77 +58,69 @@ $(document).ready(function () {
 	});
 });
 	
-function myGetJSON(sourceArray) 
-    {
-    if(sourceArray[0]=='hackernews') 
-		{
-        var j = 1;
-		$.getJSON(
-			sourceArray[3] + ".json?", //"limitToFirst=" +sourceArray[2],
-			function parseJSON(data)
-				{
-				$("." +sourceArray[1]+ "content").empty();
-				$.each(data.slice(0, sourceArray[2]), function (i, post) 
-					{ 
-					hackerAppend(sourceArray, post, j);
-					j += 1;
-					});
-				}
-			);
-		}
-    
-    else {
-      $.getJSON(
-          sourceArray[3] + ".json?limitToFirst=5", //jsonp=?
-          function parseJSON(data)
-          {
-            //console.log("getting");
-            $("." +sourceArray[1]+ "content").empty();
-            $.each(data.data.children.slice(0, sourceArray[2]), function (i, post) 
-              {
-                $("." +sourceArray[1]+ "content").append("<a href='http://www.reddit.com" +post.data.permalink+ "' rel='nofollow'><img src='cm.gif' border='0'></a>&nbsp; <a href='" +post.data.url+ "' rel='nofollow' class='bl'>" +post.data.title+ "</a><br>");
-                $("." +sourceArray[1]+ "content").append("<div class='divider'></div>");
-              }
-            );
-          $("." +sourceArray[1]+ "content").append("<div class='source'>&nbsp;More at&nbsp;<a href='" +sourceArray[3]+ "' rel='nofollow' class='asource'>" +sourceArray[4]+ "</a></div>");
-          }
-        );
+function myGetJSON(sourceArray) {
+  if(sourceArray[0]=='hackernews') {
+    var j = 1;
+    $.getJSON(
+      sourceArray[3] + ".json?", //"limitToFirst=" +sourceArray[2],
+      function parseJSON(data) {
+        $("." +sourceArray[1]+ "content").empty();
+        $.each(data.slice(0, sourceArray[2]), function (i, post) {
+          hackerAppend(sourceArray, post, j);
+          j += 1;
+        });
       }
-    }
+    );
+  }
+  else {
+    $.getJSON(
+      sourceArray[3] + ".json?limitToFirst=5", //jsonp=?
+      function parseJSON(data) {
+        //console.log("getting");
+        $("." +sourceArray[1]+ "content").empty();
+        $.each(data.data.children.slice(0, sourceArray[2]), function (i, post) {
+          $("." +sourceArray[1]+ "content").append("<a href='http://www.reddit.com" +post.data.permalink+ "' rel='nofollow'><img src='graphics/cm.gif' border='0'></a>&nbsp; <a href='" +post.data.url+ "' rel='nofollow' class='bl'>" +post.data.title+ "</a><br>");
+          $("." +sourceArray[1]+ "content").append("<div class='divider'></div>");
+        });
+        $("." +sourceArray[1]+ "content").append("<div class='source'>&nbsp;More at&nbsp;<a href='" +sourceArray[3]+ "' rel='nofollow' class='asource'>" +sourceArray[4]+ "</a></div>");
+      }
+    );
+  }
+}
 
 function hackerAppend(sourceArray, post, j) {
   $.getJSON(sourceArray[4] +post+ ".json?",
-    function parseJSON(data) 
-    {
-    $("." +sourceArray[1]+ "content").append("<a href='https://news.ycombinator.com/item?id=" +data.id+ "' rel='nofollow'><img src='cm.gif' border='0'></a>&nbsp; <a href='" +data.url+ "' rel='nofollow' class='bl'>" +data.title+ "</a><br>");
-    $("." +sourceArray[1]+ "content").append("<div class='divider'></div>");
-    if (j == sourceArray[2]) {
-      $("." +sourceArray[1]+ "content").append("<div class='source'>&nbsp;More at&nbsp;<a href='" +sourceArray[4]+ "' rel='nofollow' class='asource'>" +sourceArray[5]+ "</a></div>");
+    function parseJSON(data) {
+      $("." +sourceArray[1]+ "content").append("<a href='https://news.ycombinator.com/item?id=" +data.id+ "' rel='nofollow'><img src='graphics/cm.gif' border='0'></a>&nbsp; <a href='" +data.url+ "' rel='nofollow' class='bl'>" +data.title+ "</a><br>");
+      $("." +sourceArray[1]+ "content").append("<div class='divider'></div>");
+      if (j == sourceArray[2]) {
+        $("." +sourceArray[1]+ "content").append("<div class='source'>&nbsp;More at&nbsp;<a href='" +sourceArray[4]+ "' rel='nofollow' class='asource'>" +sourceArray[5]+ "</a></div>");
       }
-    
     }
   );
 }
 
-function myGetNYTimes(sourceArray) 
-  {
+function myGetNYTimes(sourceArray) {
   var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
   url += '?' + $.param({
-    'api-key': "3e6e052bf4bc4dc69a4d2236ecf8b89c",
+    'api-key': "3b20fe86a1714372960f3048fe35b399",
     'limitToFirst': 10
-  });  
+  });
   $.ajax({
     url: url,
     method: 'GET',
-    }).done(function(result) {
-      $("." +sourceArray[1]+ "content").empty();
-      for (var i = 0; i < sourceArray[2]; i++) {
-        //console.log(result["results"][i]["title"]);
-        $("." +sourceArray[1]+ "content").append("<a href='" +result["results"][i]["url"]+ "' rel='nofollow'><img src='cm.gif' border='0'></a>&nbsp; <a href='" +result["results"][i]["url"]+ "' rel='nofollow' class='bl'>" +result["results"][i]["title"]+ "</a><br>");
-        $("." +sourceArray[1]+ "content").append("<div class='divider'></div>");
-      }
-      $("." +sourceArray[1]+ "content").append("<div class='source'>&nbsp;More at&nbsp;<a href='" +sourceArray[2]+ "' rel='nofollow' class='asource'>" +sourceArray[3]+ "</a></div>");
-    }).fail(function(err) {
-      throw err;
+    //dataType: 'jsonp'
+  })
+  .done(function(result) {
+    $("." +sourceArray[1]+ "content").empty();
+    for (var i = 0; i < sourceArray[2]; i++) {
+      //console.log(result["results"][i]["title"]);
+      $("." +sourceArray[1]+ "content").append("<a href='" +result["results"][i]["url"]+ "' rel='nofollow'><img src='graphics/cm.gif' border='0'></a>&nbsp; <a href='" +result["results"][i]["url"]+ "' rel='nofollow' class='bl'>" +result["results"][i]["title"]+ "</a><br>");
+      $("." +sourceArray[1]+ "content").append("<div class='divider'></div>");
+    }
+    $("." +sourceArray[1]+ "content").append("<div class='source'>&nbsp;More at&nbsp;<a href='" +sourceArray[2]+ "' rel='nofollow' class='asource'>" +sourceArray[3]+ "</a></div>");
+  })
+  .fail(function(err) {
+    throw err;
   });
- }
+}
